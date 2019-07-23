bf1b40ae676935c25eb0bcf775e08aae4b082f41
Ethanleba <leba.e@husky.neu.edu>
2019-07-23 12:23:37 -0400

    shifted unnecessary code out of filemanager

--- src/filemanager.py
+++ src/filemanager.py
@@ -1,31 +1,4 @@
-from .transition_matrix import nd_convert_to_probabilities, populate_matrix, prep_list, entry_point
-from .parse_midi import parse_midi
-from .markov import NDMarkovChain
-from .util import cartesian_product
-from . import constants
 import pickle
-import random
-
-# The cartesian product of the two octaves and the possible note durations.
-scalecombo = cartesian_product(
-    [i for i in range(-7, 8)],
-    constants.note_duration)
-
-
-# Generates the transition matrix for the markov chain
-def scalecombo_matrix():
-    return nd_convert_to_probabilities(populate_matrix(
-        prep_list(parse_midi(), scalecombo), len(scalecombo), 3))
-
-
-# Chooses a primer melody for the markov chain
-def scalecombo_entries():
-    return random.choice(entry_point(prep_list(parse_midi(), scalecombo), 2))
-
-
-# The markov object used by the application
-mark = NDMarkovChain(scalecombo_matrix(), scalecombo,
-                     list(reversed(scalecombo_entries())))
 
 
 # Saves the markov chain object
@@ -40,5 +13,3 @@ def load():
 def main():
     print("Markov chain has been updated at " + constants.save_matrix_path)
     save(mark)
-
-main()
--- src/run.py
+++ src/run.py
@@ -3,6 +3,26 @@ from .filemanager import load
 from .output.midi import generate_midi
 from .output.json import generate_json
 
+# The cartesian product of the two octaves and the possible note durations.
+scalecombo = cartesian_product(
+    [i for i in range(-7, 8)],
+    constants.note_duration)
+
+
+# Generates the transition matrix for the markov chain
+def scalecombo_matrix():
+    return nd_convert_to_probabilities(populate_matrix(
+        prep_list(parse_midi(), scalecombo), len(scalecombo), 3))
+
+
+# Chooses a primer melody for the markov chain
+def scalecombo_entries():
+    return random.choice(entry_point(prep_list(parse_midi(), scalecombo), 2))
+
+
+# The markov object used by the application
+mark = NDMarkovChain(scalecombo_matrix(), scalecombo,
+                     list(reversed(scalecombo_entries())))
 
 def main():
     return generate_json(load())
